344e6dfcbf44643418403bc9e3ba70ff6705d472
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-11-03 16:05:52 +0400

    progres bars

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 new Vue({
   el: "#exercise",
   data: {
+    atachRed: false,
     modelName: "giorgi",
     counter: 0,
     secondCounter: 0,
-    value: 0
+    value: 0,
+    color: "green",
+    effectClasses: {
+      red: true,
+      green: false
+    },
+
+    width: 10
   },
 
   computed: {
+    stylechanger: function() {
+      vm = this;
+      var widthX = vm.width;
+      setInterval(() => {
+        vm.width = widthX + "px";
+        widthX += 10;
+      }, 300);
+    },
     outpoot: function() {
       console.log("computed");
       return this.counter > 10 ? "more than ten" : "less then then";
     },
+    colorChanger: function() {
+      var xn = this;
+      setInterval(() => {
+        xn.effectClasses.red = !xn.effectClasses.red;
+        xn.effectClasses.green = !xn.effectClasses.green;
+      }, 1000);
+    },
     result: function() {
       return this.value == 37 ? "not there" : "there";
+    },
+    divClasses: function() {
+      return {
+        red: this.atachRed,
+        blue: !this.atachRed
+      };
     }
   },
   watch: {
