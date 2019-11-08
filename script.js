48b8d086146423e35ff65d882ff020a24de705e9
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-11-08 23:01:50 +0400

    v-for , Array.isArray(data)

--- script.js
+++ script.js
@@ -1,63 +1,18 @@
 new Vue({
   el: "#exercise",
   data: {
-    atachRed: false,
-    modelName: "giorgi",
-    counter: 0,
-    secondCounter: 0,
-    value: 0,
-    color: "green",
-    effectClasses: {
-      red: true,
-      green: false
-    },
-
-    width: 10
-  },
-
-  computed: {
-    stylechanger: function() {
-      vm = this;
-      var widthX = vm.width;
-      setInterval(() => {
-        vm.width = widthX + "px";
-        widthX += 10;
-      }, 300);
-    },
-    outpoot: function() {
-      console.log("computed");
-      return this.counter > 10 ? "more than ten" : "less then then";
-    },
-    colorChanger: function() {
-      var xn = this;
-      setInterval(() => {
-        xn.effectClasses.red = !xn.effectClasses.red;
-        xn.effectClasses.green = !xn.effectClasses.green;
-      }, 1000);
-    },
-    result: function() {
-      return this.value == 37 ? "not there" : "there";
-    },
-    divClasses: function() {
-      return {
-        red: this.atachRed,
-        blue: !this.atachRed
-      };
-    }
-  },
-  watch: {
-    value: function() {
-      var vm = this;
-      setTimeout(function() {
-        vm.value = 0;
-      }, 2000);
-    }
-  },
-
-  methods: {
-    results: function() {
-      console.log("method");
-      return this.counter > 10 ? "more than ten" : "less then then";
+    show: true,
+    clas: "red",
+    message: "hey",
+    ingridients: ["banan", "apple", "garlick"],
+    persons: [
+      { name: "toko", age: 33, color: "red" },
+      { name: "gui", age: 23, color: "yellow" }
+    ],
+    fourth: {
+      name: "TESTOBJECT",
+      data: [1.67, 1.33, 0.98, 2.21],
+      surname: "zhizhi"
     }
   }
 });
