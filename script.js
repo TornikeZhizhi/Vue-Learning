52422fceee10fc42d9e2332bc82375a2af752ba1
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2019-11-11 00:48:30 +0400

    ariol

--- script.js
+++ script.js
@@ -1,117 +1,51 @@
 new Vue({
-  el: "#exercise",
+  el: "#app",
   data: {
-    yourScore: 0,
-    monsterScore: 0,
-    name: "niklesh",
-    otherclients: [
-      {
-        doc: {
-          notes: [
-            {
-              id: "w5fpn80fnnf5nxdj9f1n1i",
-              text: "Welcome new device mydoc_android",
-              owner: "YOU",
-              deleted: false
-            },
-            {
-              id: "w5fpn80fnnf5nxdj9f1wwwn1i",
-              text: "android 2",
-              owner: "YOU",
-              deleted: false
-            }
-          ]
-        }
-      },
-      {
-        doc: {
-          notes: [
-            {
-              id: "c1ds7zqd7tcgig0b1xs1q",
-              text: "Welcome new device mydoc_ios",
-              owner: "YOU",
-              deleted: false
-            },
-            {
-              id: "nf5nxdj9f1dwwen1iw5fpn80fn",
-              text: "More Text",
-              owner: "YOU",
-              deleted: false
-            }
-          ]
-        }
-      }
-    ],
-
-    show: true,
-    YouTrue: false,
-    MonsterTrue: false,
-    myWidth: 100,
-    monsterWidth: 100,
-    damage: {
-      mydamage: 0,
-      monsterDamage: 0
-    }
+    front: true,
+    winScore: 5,
+    gameRunning: true,
+    MyInfo: null,
+    EnemyInfo: null,
+    frontScore: 0,
+    backScore: 0
   },
-
   methods: {
-    attackFunction: function(e) {
-      var myRandom = Math.floor(Math.random() * 15);
-      var monsterRandom = Math.floor(Math.random() * 15);
-
-      this.damage.mydamage = monsterRandom;
-      this.damage.monsterDamage = myRandom;
-
-      document.getElementById("mydamage").innerHTML +=
-        "<h3> You Hit - " + monsterRandom + "</h3>";
-
-      document.getElementById("monsterDamage").innerHTML +=
-        "<h3> You Hit - " + myRandom + "</h3>";
-
-      myWidthIndex = this.myWidth - myRandom;
-      monsterWidthIndex = this.monsterWidth - monsterRandom;
-
-      this.myWidth = myWidthIndex;
-      this.monsterWidth = monsterWidthIndex;
-      if (this.myWidth <= 0 || this.monsterWidth <= 0) {
-        if (this.myWidth <= this.monsterWidth) {
-          this.MonsterTrue = true;
-          this.show = !this.show;
-          this.myWidth = 100;
-          this.monsterWidth = 100;
-          this.monsterScore++;
-        } else {
-          this.YouTrue = true;
-          this.show = !this.show;
-          this.myWidth = 100;
-          this.monsterWidth = 100;
-          this.yourScore++;
-        }
+    gameStart: function(event) {
+      this.gameRunning = false;
+      var ChooseSide = event.currentTarget.innerText;
+      if (ChooseSide == "Front") {
+        this.MyInfo = "You Are Front Your Score Is";
+        this.EnemyInfo = "Enemy is Back";
+      } else {
+        this.MyInfo = "You Are Back' + Your Score Is";
+        this.EnemyInfo = "Enemy is Front";
       }
     },
+    playHandler: function() {
+      var randomIndex = Math.floor(Math.random() * 2);
 
-    specialAttack: function() {
-      var monsterRandom = Math.floor(Math.random() * 15);
-      this.monsterWidth -= 30;
-      this.myWidth -= monsterRandom;
-    },
+      if (randomIndex == 0) {
+        this.front = true;
+        this.frontScore++;
 
-    healHendler: function() {
-      var monsterRandom = Math.floor(Math.random() * 15);
-      var HealRandom = Math.floor(Math.random() * 20);
-
-      var dd = this;
-      dd.myWidth += HealRandom;
-
-      setTimeout(function() {
-        dd.myWidth -= monsterRandom;
-      }, 500);
-    },
-    giveUpfunction: function() {
-      this.show = !this.show;
-      this.myWidth = 100;
-      this.monsterWidth = 100;
-      this.monsterScore++;
+        if (this.frontScore == this.winScore) {
+          if (confirm("You won! New Game?")) {
+            this.gameRunning = true;
+            this.frontScore = 0;
+            this.backScore = 0;
+          }
+        }
+      } else {
+        this.backScore++;
+        this.front = false;
+        if (this.backScore == this.winScore) {
+          if (confirm("You Lost! New Game?")) {
+            this.gameRunning = true;
+            this.frontScore = 0;
+            this.backScore = 0;
+          }
+        }
+      }
     }
   }
 });
